97cda8d944d4d7055d29fa2713bf02a6c772f10e
Sean Murphy <sean@iamseanmurphy.com>
2026-07-06 09:08:58 +0000

    4.2.9

--- optimole-wp/vendor/composer/installed.php
+++ optimole-wp/vendor/composer/installed.php
@@ -29,9 +29,9 @@
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.53',
-            'version' => '3.3.53.0',
-            'reference' => 'a657eaedf62cc84c82e539167ac9e19b3e618ce8',
+            'pretty_version' => '3.3.54',
+            'version' => '3.3.54.0',
+            'reference' => '095c2d0f1388af0b0196c492a7f79e2fd092dab1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
